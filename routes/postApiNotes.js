4d758375d06fd90f3668c3fd25aaf7f031f15366
wolfjfly <104739255+wolfjfly@users.noreply.github.com>
2022-11-12 14:46:16 -0600

    more

--- routes/postApiNotes.js
+++ routes/postApiNotes.js
@@ -1,6 +1,17 @@
 const router = require('express').Router();
+const fs = require("fs");
 
+router.post('/animals', (req, res) => {
+    // set id based on what the next index of the array will be
+    req.body.id = animals.length.toString();
 
+    if (!validateAnimal(req.body)) {
+    res.status(400).send('The animal is not properly formatted.');
+    } else {
+    const animal = createNewAnimal(req.body, animals);
+    res.json(animal);
+    }
+});
 
 
 
