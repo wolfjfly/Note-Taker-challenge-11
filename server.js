99195018a54c855d7307b3c74144e437529fc38f
wolfjfly <104739255+wolfjfly@users.noreply.github.com>
2022-11-12 15:55:29 -0600

    1

--- server.js
+++ server.js
@@ -1,30 +1,26 @@
 const express=require('express')
+const path = require('path');
 const app= express()
-const apiRoutes = require('./routes');
+const apiRoutes = require('./routes/index.js');
 const PORT=process.env.PORT || 3000
 
-app.use(logger)
 
-app.use('/pathfinder', function(req, res, next){
-	pathfinderUI(app)
-	next()
-}, pathfinderUI.router)
-
-app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-
+app.use(express.urlencoded({ extended: true }))
+app.use('/api', apiRoutes);
 app.use(express.static("public"))
 
-app.use('/api', apiRoutes);
 
-app.get("*", function(req, res){
-	res.redirect('/pathfinder');
-})
 
-function logger(req, res, next){
-    console.log(req.originalUrl)
-    next()
-}
+// GET Route for homepage
+app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.get("/notes", (req, res) =>
+res.sendFile(path.join(__dirname, "/public/notes.html"))
+);
+
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
