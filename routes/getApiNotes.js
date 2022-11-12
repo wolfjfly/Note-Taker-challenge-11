c9c09b6f96bba65437255a7d7df0378f95026f5b
wolfjfly <104739255+wolfjfly@users.noreply.github.com>
2022-11-12 14:46:07 -0600

    starting the js routes

--- routes/getApiNotes.js
+++ routes/getApiNotes.js
@@ -1,6 +1,21 @@
 const router = require('express').Router();
 
+router.get('/animals', (req, res) => {
+    let results = animals;
+    if (req.query) {
+    results = filterByQuery(req.query, results);
+    }
+    res.json(results);
+});
 
+router.get('/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+    res.json(result);
+    } else {
+    res.send(404);
+    }
+});
 
 
 
--- server.js
+++ server.js
@@ -1,15 +1,25 @@
 const express=require('express')
 const app= express()
+const apiRoutes = require('./routes');
 const PORT=process.env.PORT || 3000
-
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
-app.use(express.static("public"))
 
 app.use(logger)
 
-g_api(app)
-p_api(app)
+app.use('/pathfinder', function(req, res, next){
+	pathfinderUI(app)
+	next()
+}, pathfinderUI.router)
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.use(express.static("public"))
+
+app.use('/api', apiRoutes);
+
+app.get("*", function(req, res){
+	res.redirect('/pathfinder');
+})
 
 function logger(req, res, next){
     console.log(req.originalUrl)
